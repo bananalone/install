fe4939341c6b385e6225079965ff397d81f0d707
bananalone <yx243354200@qq.com>
2022-09-07 15:34:59 +0800

    fix: vim GITHUB_ROOT

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,4 @@
-export GITHUB_ROOT={$GITHUB_ROOT:-'https://ghproxy.com/https://raw.githubusercontent.com/bananalone/install/tree/main'}
+export GITHUB_ROOT={$GITHUB_ROOT:-'https://ghproxy.com/https://raw.githubusercontent.com/bananalone/install/main'}
 
 cd ~
 apt install -y vim && \
