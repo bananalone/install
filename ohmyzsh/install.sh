a7fc86d4f2765b68d90928071e519f23aa4fdaf9
bananalone <yx243354200@qq.com>
2022-09-07 17:03:15 +0800

    fix: ohmyzsh add apt install -y git

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -1,5 +1,5 @@
 cd ~
-apt install -y zsh && \
+apt install -y git zsh && \
 export REMOTE=https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh.git && \
 echo "n" | /bin/bash -c "$(curl -fsSL https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh/blob/master/tools/install.sh)" && \
 chsh -s /bin/zsh
