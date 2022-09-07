2675c8ce1333a90e5de6cbfdb80e4d28f123fcc4
bananalone <yx243354200@qq.com>
2022-09-07 18:26:36 +0800

    fix python add ln -s

--- python/install.sh
+++ python/install.sh
@@ -1,4 +1,5 @@
 cd ~
 apt install -y python3-pip && \
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple && \
-pip install --upgrade pip
+pip install --upgrade pip && \
+ln -s "$(which python3)" /bin/python
