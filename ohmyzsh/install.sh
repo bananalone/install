4efeea52816a7581a80599f90cdd5e94b5d693db
bananalone <yx243354200@qq.com>
2022-09-11 00:42:03 +0800

    add zshrc, mod ohmyzsh|vim/install.sh if command exists or not

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -1,5 +1,12 @@
 cd ~
-apt install -y git zsh && \
-export REMOTE=https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh.git && \
-echo "n" | /bin/bash -c "$(curl -fsSL https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh/blob/master/tools/install.sh)" && \
+
+if [ ! command -v zsh > /dev/null 2>&1 ]; then
+    apt install -y git zsh
+fi
+
+export REMOTE=https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh.git
+echo "n" | /bin/bash -c "$(curl -fsSL https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh/blob/master/tools/install.sh)"
+git clone https://ghproxy.com/https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://ghproxy.com/https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/bananalone/install/main/ohmyzsh/.zshrc -o $HOME/.zshrc
 chsh -s /bin/zsh
--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,7 @@
 cd ~
-apt install -y vim && \
+
+if [ ! command -v zsh > /dev/null 2>&1 ]; then
+    apt install -y vim
+fi
+
 curl -fsSL "https://ghproxy.com/https://raw.githubusercontent.com/bananalone/install/main/vim/.vimrc" -o ~/.vimrc
