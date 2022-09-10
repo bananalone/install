b4e7ebdaedcb45d67fc26bce9624f16d66e0973a
bananalone <yx243354200@qq.com>
2022-09-11 01:44:42 +0800

    fix ohmyzsh can not clone zsh-syntax-highlighting

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -1,12 +1,16 @@
-cd ~
+ cd ~
 
-if ! command -v zsh > /dev/null 2>&1; then
-    apt install -y git zsh
-fi
+ if ! command -v zsh > /dev/null 2>&1; then
+     apt install -y zsh
+ fi
 
-export REMOTE=https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh.git
-echo "n" | /bin/bash -c "$(curl -fsSL https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh/blob/master/tools/install.sh)"
-git clone https://ghproxy.com/https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://ghproxy.com/https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/bananalone/install/main/ohmyzsh/.zshrc -o $HOME/.zshrc
-chsh -s /bin/zsh
+ if [ -d $HOME/.oh-my-zsh ]; then
+     rm -rf $HOME/.oh-my-zsh
+ fi
+
+ export REMOTE=https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh.git
+ echo "n" | /bin/bash -c "$(curl -fsSL https://ghproxy.com/https://github.com/ohmyzsh/ohmyzsh/blob/master/tools/install.sh)"
+ git clone https://ghproxy.com/https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+ git clone https://ghproxy.com/https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+ curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/bananalone/install/main/ohmyzsh/.zshrc -o $HOME/.zshrc
+ chsh -s /bin/zsh
